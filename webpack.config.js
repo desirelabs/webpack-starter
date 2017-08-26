7e60764e4d7b1842a53078807fe343cdeb964bc7
franck <neovea@gmail.com>
2017-08-26 15:52:10 +0200

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,76 @@
+const path = require('path')
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin')
+const ExtractTextPlugin = require("extract-text-webpack-plugin")
+const dev = process.env.NODE_ENV === 'dev'
+
+let cssLoaders = [
+  {loader: 'css-loader', options: {
+    importLoaders: 1,
+    minimize: !dev
+  }}
+]
+
+if (!dev) {
+  cssLoaders.push({
+    loader: 'postcss-loader',
+    options: {
+      plugins: (loader) => [
+        require('autoprefixer')({
+          "browsers": ["last 2 versions", "safari >= 7", "ie >= 9", "ios >= 6"]
+        })
+      ]
+    }
+  })
+}
+
+let config = {
+  entry: {
+    app: './assets/js/app.js'
+  },
+  watch: dev,
+  devtool: dev ? "cheap-module-eval-source-map" : false,
+  output: {
+    path: path.resolve('./dist/'),
+    filename: '[name].js',
+    publicPath: "/dist/"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        exclude: /(node_modules|bower_components)/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: cssLoaders
+        })
+      },
+      {
+        test: /\.scss$/,
+        exclude: /(node_modules|bower_components)/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: [...cssLoaders, 'sass-loader']
+        })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: '[name].css',
+      disable: dev
+    })
+  ]
+}
+
+if (!dev) {
+  config.plugins.push(new UglifyJSPlugin({
+    sourceMap: false
+  }))
+}
+
+module.exports = config
