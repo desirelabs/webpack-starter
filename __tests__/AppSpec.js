0006df89dfa31603d1f1f97d340c95908a23b090
Franck <f.lebas@inovalia.fr>
2017-10-03 15:04:25 +0200

    wrote basic suit of tests with jest and enzyme, implemented eslint jest plugin

--- __tests__/AppSpec.js
+++ __tests__/AppSpec.js
@@ -1,11 +1,50 @@
 import React from 'react'
 import App from '../assets/js/react/App'
-import renderer from 'react-test-renderer'
+import Adapter from 'enzyme-adapter-react-15'
+import { configure, mount, shallow } from 'enzyme'
 
-test('Link changes the class when hovered', () => {
-  const component = renderer.create(
-    <App name='React' />
-  )
-  let tree = component.toJSON()
-  expect(tree).toMatchSnapshot()
+configure({ adapter: new Adapter() })
+
+describe('App', () => {
+  // Component's Boilerplate
+  let props
+  let mountedApp
+  const app = () => {
+    if (mountedApp) {
+      mountedApp = mount(<App {...props} />)
+    }
+    return mountedApp
+  }
+
+  beforeEach(() => {
+    props = {
+      name: undefined
+    }
+    mountedApp = undefined
+  })
+
+  // Tests
+  it('Always render a title tag h1', () => {
+    const wrapper = shallow(<App />)
+    const title = wrapper.find('h1')
+    expect(title.length).toBeGreaterThan(0)
+  })
+
+  it('Expects title tag content to exist', () => {
+    const wrapper = shallow(<App />)
+    const title = wrapper.find('h1')
+    expect(title.text().length).toBeGreaterThan(0)
+  })
+
+  it('Expects title tag content to contain `Hello`', () => {
+    const wrapper = shallow(<App />)
+    const title = wrapper.find('h1')
+    expect(title.text().indexOf('Hello')).toBeGreaterThan(-1)
+  })
+
+  it('Expects title tag content to contain `React`', () => {
+    const wrapper = mount(<App />)
+    const title = wrapper.find('h1')
+    expect(title.text().indexOf('React')).toBeGreaterThan(-1)
+  })
 })
--- assets/js/react/App.js
+++ assets/js/react/App.js
@@ -1,9 +1,28 @@
 import React from 'react'
 
 class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      name: ''
+    }
+
+    this.setData = this.setData.bind(this)
+  }
+
+  componentWillMount () {
+    this.setData()
+  }
+
+  setData () {
+    this.setState({
+      name: 'React'
+    })
+  }
+
   render () {
     return (
-      <h1>Hello !</h1>
+      <h1>Hello {this.state.name}!</h1>
     )
   }
 }
